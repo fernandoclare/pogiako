d078e805234d97792e3e30df8b5cd6bbb863917d
fernandoclare <fernandoclarenceadrian@gmail.com>
2024-05-30 16:41:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["Student", "Gamer", "Part-time Cook","Content Creator"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true,
+});
